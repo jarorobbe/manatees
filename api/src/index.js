51c67d87be74ba81d49ee97140e36b2f973a9346
jarorobbe <jarorobbe@hotmail.com>
2021-03-30 22:14:12 +0200

    feat: cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const championRouter = require('./routes/championRoutes');
 
 const app = express();
 const port = process.env.PORT ? process.env.PORT : 3001;
 
+app.use(cors());
 app.use('/api/champion', championRouter);
 
 app.listen(port, () => {
